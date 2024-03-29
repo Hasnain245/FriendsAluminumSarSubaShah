29e6196af048c03b2337519b828c70ee0979bf59
Hasnain245 <mirzahasnain743@gmail.com>
2024-03-29 15:12:39 +0500

    second commit

--- js/script.js
+++ js/script.js
@@ -13,3 +13,47 @@ function menutoggle() {
         menuitems.style.maxHeight = "0px";
     }
 }
+
+
+
+// RealTime Database Through Firebase Script 
+
+  // Import the functions you need from the SDKs you need
+  // import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
+  // import { getDatabase, ref, set, get, child } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-database.js";
+  // import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  // const firebaseConfig = {
+  //   apiKey: "AIzaSyBRhJezJm0V72DH2xq-KjFD9EAlQIqUo2E",
+  //   authDomain: "friendsaluminum-sss.firebaseapp.com",
+  //   projectId: "friendsaluminum-sss",
+  //   storageBucket: "friendsaluminum-sss.appspot.com",
+  //   messagingSenderId: "1013359922230",
+  //   appId: "1:1013359922230:web:606cc16c38b16434219c97",
+  //   measurementId: "G-969MJDTY62"
+  // };
+
+  // Initialize Firebase
+  // const app = initializeApp(firebaseConfig);
+  // const analytics = getAnalytics(app);
+
+  // const db = getDatabase(app);
+
+  // document.getElementById("submit").addEventListener('click', function(e){
+  //   e.preventDefault();
+  //   set(ref(db, 'user/' + document.getElementById("username").value),{
+
+  //       username: document.getElementById("username").value,
+  //       email: document.getElementById("email").value,
+  //       subject: document.getElementById("subject").value,
+  //       textarea: document.getElementById("textarea").value
+
+  //   });
+  //   alert("Submitted Successfully  !");
+  // })
+
+ 
